37d9f532c122906c1f7d95f72d6d37ad2a8387cf
Shirishapathulothu <pathulothushirisha@gmail.com>
2025-01-15 17:37:03 +0530

    condition ex

--- script-ex13.sh
+++ script-ex13.sh
@@ -4,7 +4,7 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-sudo mkdir -p /var/log/shell-script
+mkdir -p $LOGS_FOLDER
 
 Userid=$(id -u)
   echo "User id is: $Userid"
